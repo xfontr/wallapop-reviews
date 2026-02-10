145866093dc8842f9cc42d594792235a074b3aa4
Xifré Font <xifre.font.97@gmail.com>
2026-02-10 18:47:38 +0100

    feat(3): Delete React

--- src/options/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <>This is the options view</>;
-}
-
-export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 // vite.config.ts
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()],
   build: {
     rollupOptions: {
       input: {
